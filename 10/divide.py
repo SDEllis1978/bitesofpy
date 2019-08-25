1311115f8b0161bf680526e1c424739126acc650
sdellis1978 <sdellis1978@gmail.com>
2019-08-25 15:22:45 -0400

    Added to working branch - learning exception handling
    Signed-off-by: sdellis1978 <sdellis1978@gmail.com>

--- 10/divide.py
+++ 10/divide.py
@@ -5,6 +5,9 @@ def positive_divide(numerator, denominator):
         n = numerator / denominator
     except ZeroDivisionError as e:
         return 0
+    try:
+        numerator = int(numerator)
+        denominator = int(denominator)
     except TypeError:
         if not isinstance(numerator, (int, float)) and not isinstance(denominator, (int, float)):
             raise
